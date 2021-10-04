3c75731f5442718fb1f5a97ea7b4ad8781ea8f00
abrar-hossain <abrarmashuk@gmail.com>
2021-10-04 21:33:56 +0600

    url added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,40 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 const Footer = () => {
     return (
 
-
-        <div style={{ backgroundColor: "black", color: "white", height: '100px' }}>
-            <h5 className="container text-center pt-5">Copyright © 2008 Apple Inc. All rights reserved</h5>
-                </div>
-
-
+        <Container className="mt-5 pt-5" style={{ backgroundColor: "#0063B2FF", color: "white" }}>
+            <Row>
+                <Col>
+                    <h4>Our Branches</h4>
+                    <p>Dhaka</p>
+                    <p>Chattagram</p>
+                    <p>Sylhet</p>
+                    <p>Rajshahi</p>
+                </Col>
+                <Col>
+                    <h4>Important Links</h4>
+                    <p>Contact US</p>
+                    <p>Newsletter</p>
+                    <p>Career</p>
+                </Col>
+                <Col>
+                    <h4>Subscribe</h4>
+                    <br />
+                    <h6>Email</h6>
+                    <div className="search-service">
+                        <input className="p-2" type="text" />{" "}
+                        <button className="btn p-2 btn-dark">Search</button>
+                    </div>
+                </Col>
+            </Row>
+            <hr />
+            <div>
+                <h5 className="text-center pb-5">Copyright © 2021  All rights reserved</h5>
+            </div>
+        </Container>
     );
 };
 
